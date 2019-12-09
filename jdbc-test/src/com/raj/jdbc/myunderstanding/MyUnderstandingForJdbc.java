b02f5f6df49e5dcdec658770f8b075038e91735c
rajkhare <rajkhare@192.168.31.81>
2019-12-09 20:34:36 +0530

    update for myUnderstanding and jdbc.docx

--- jdbc-test/src/com/raj/jdbc/myunderstanding/MyUnderstandingForJdbc.java
+++ jdbc-test/src/com/raj/jdbc/myunderstanding/MyUnderstandingForJdbc.java
@@ -12,6 +12,10 @@ public class MyUnderstandingForJdbc {
 		String dbUrl = "jdbc:mysql://localhost:3306/demo";
 		String name = "student2";
 		String pass = "student2";
+		
+		//Searching parameters
+		String firstName="Raj";
+		String lastName="Khare";
 
 		try {
 			// 1. Create the connection
@@ -19,17 +23,23 @@ public class MyUnderstandingForJdbc {
 			myConn = DriverManager.getConnection(dbUrl, name, pass);
 			System.out.println("Connection created!");
 			
-			System.out.println("Before update the deatils for Employee LastName = Khare , FirstName = Raj");
+			System.out.println("Before update the deatils searching for Employee LastName = "+lastName+", FirstName = "+firstName+"...");
 
-			displayDetaisForEmployee(myConn, myStmt, myRs, "Khare", "Raj");
+			displayDetaisForEmployee(myConn, myStmt, myRs, lastName, firstName);
 			
 			System.out.println("Updating the details...");
 
-			updateDetailsForEmployee("1.raj.khare@gmail.com", "Raj", "Khare", myConn, myStmt);
+			updateDetailsForEmployee("1.raj.khare@gmail.com", firstName, lastName, myConn, myStmt);
 			
-			System.out.println("After update the deatils for Employee LastName = Khare , FirstName = Raj");
+			System.out.println("After update the deatils searching for Employee LastName = "+lastName+", FirstName = "+firstName+"...");
 			
-			displayDetaisForEmployee(myConn, myStmt, myRs, "Khare", "Raj");
+			displayDetaisForEmployee(myConn, myStmt, myRs, lastName, firstName);
+			
+			System.out.println("Deleting the Employee LastName = "+lastName+", FirstName= "+firstName);
+			deleteEmployee(myConn,myStmt,lastName,firstName);
+			
+			System.out.println("After delete the deatils searching for Employee LastName = "+lastName+", FirstName = "+firstName+"...");
+			displayDetaisForEmployee(myConn, myStmt, myRs, lastName, firstName);
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -48,6 +58,27 @@ public class MyUnderstandingForJdbc {
 
 	}
 
+	private static void deleteEmployee(Connection myConn, PreparedStatement myStmt, String lastName, String firstName) {
+		try {
+			//prepare statement
+			myStmt = myConn.prepareStatement("delete from employees"
+					+ " where last_name=? and first_name=?");
+			
+			//set param
+			myStmt.setString(1, lastName);
+			myStmt.setString(2, firstName);
+			
+			//execute query
+			int rowAffected = myStmt.executeUpdate();
+			System.out.println("No. of Rows Affected: "+rowAffected);
+			System.out.println("Record DELETED!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 	private static void displayDetaisForEmployee(Connection myConn, PreparedStatement myStmt, ResultSet myRs,
 			String lastName, String firstName) {
 		try {
@@ -60,11 +91,20 @@ public class MyUnderstandingForJdbc {
 			myRs = myStmt.executeQuery();
 
 			// process result set
+			boolean found = false;
 			while (myRs.next()) {
-				System.out.println(myRs.getInt("id") + " | " + myRs.getString("last_name") + " | "
+				System.out.printf("Found employee: %s %s\n", firstName, lastName);
+				System.out.println("Details are:");
+				System.out.println(+myRs.getInt("id") + " | " + myRs.getString("last_name") + " | "
 						+ myRs.getString("first_name") + " | " + myRs.getString("email") + " | "
 						+ myRs.getString("department") + " | " + myRs.getString("salary"));
+				found = true;
 			}
+			
+			if(!found) {
+				System.out.println("Employee NOT FOUND: "+firstName+" "+lastName);
+			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
