78a3fe0c69e27823a9b4a486d07636005f430019
RK00333040 <RK00333040@TMIC-LPT013815.TMHaier.ad>
2019-12-14 17:58:20 +0530

    update for configuration file

--- jdbc-test/src/com/raj/jdbc/propertiesfiles/ConnectionDemo.java
+++ jdbc-test/src/com/raj/jdbc/propertiesfiles/ConnectionDemo.java
@@ -4,13 +4,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
 public class ConnectionDemo {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		 
 		Connection myConn = null;
 		Statement myStmt = null;
@@ -27,16 +29,41 @@ public class ConnectionDemo {
 			String theUser = props.getProperty("user");
 			String thePassword = props.getProperty("password");
 			
-			System.out.println();
+			System.out.println("Connection to databae...");
+			System.out.println("Dabase URL: "+theDburl);
+			System.out.println("User: "+theUser);
 			
-			//3. Get 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+			
+			//3. Get connection to database
+				myConn =  DriverManager.getConnection(theDburl, theUser, thePassword);
+				System.out.println("Connection established!");
+			
+			//3. Create Statement
+				myStmt = myConn.createStatement();
+				
+			//4. Execute sql query
+				myRs = myStmt.executeQuery("select * from employees");
+				
+			//Process the ResultSet
+				while(myRs.next()) {
+					System.out.println(myRs.getInt("id")+" | "+myRs.getString("last_name")+" | "+myRs.getString("first_name")+" | "+myRs.getString("email")+
+							" | "+myRs.getString("department")+" | "+myRs.getDouble("salary"));
+				}
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
+			if(myConn !=null) {
+				myConn.close();
+			}
+			
+			if(myStmt !=null) {
+				myStmt.close();
+			}
+			
+			if(myRs !=null) {
+				myRs.close();
+			}
 		}
 	}
 
